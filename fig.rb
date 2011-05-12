82ce0b768e5e7bcf61632db7a5ca74047686bf62
shunirr <shunirr@gmail.com>
2011-05-12 21:07:04 +0900

    いろいろ直した

--- fig.rb
+++ fig.rb
@@ -1,76 +1,107 @@
 #!/usr/bin/env ruby
 # vim:encoding=UTF-8:
 
-$LOAD_PATH << "lib"
-$LOAD_PATH << "../lib"
+$LOAD_PATH << 'lib'
+$LOAD_PATH << '../lib'
 
-$KCODE = "u" unless defined? ::Encoding
+$KCODE = 'u' unless defined? ::Encoding
 
-require "rubygems"
-require "net/irc"
-require "sdbm"
-require "tmpdir"
-require "uri"
+require 'rubygems'
+require 'net/irc'
+require 'sdbm'
+require 'tmpdir'
+require 'uri'
 require 'oauth'
 require 'facebook_oauth'
 require 'yaml'
 
-
 class FacebookIrcGateway < Net::IRC::Server::Session
   def server_name
-    "fig"
+    'FacebookIrcGateway'
   end
 
   def server_version
-    "0.0.0"
+    '0.0.0'
   end
 
   def main_channel
-    "#facebook"
+    '#facebook'
   end
 
   def initialize(*args)
     super
-    config = YAML::load(open('config.yaml').read)
-    agent = FacebookOAuth::Client.new(
-      :application_id     => config['app']['id'],
-      :application_secret => config['app']['secret'],
-      :callback           => config['app']['callback']
-    )
-    access_token = agent.authorize(:code => config['client']['code'])
-    @client = FacebookOAuth::Client.new(
-      :application_id     => config['app']['id'],
-      :application_secret => config['app']['secret'],
-      :token              => access_token.token
-    )
+    
+    # read config file
+    if File.exist? @opts.config then
+      config = YAML::load open(@opts.config).read
+    else
+      @log.error "Cant read #{@opts.config}"
+      exit 0
+    end
+
+    begin
+      agent = FacebookOAuth::Client.new(
+        :application_id     => config['app']['id'],
+        :application_secret => config['app']['secret'],
+        :callback           => config['app']['callback']
+      )
+    rescue Exception => e
+      @log.error e.inspect
+      e.backtrace.each do |l|
+        @log.error "\t#{l}"
+      end
+    end
+
+    # got oauth client code?
+    @setup = config['client']['code'].nil?
+
+    if @setup then
+      @client = agent
+      return
+    end
+
+    begin
+      access_token = agent.authorize(:code => config['client']['code'])
+      @client = FacebookOAuth::Client.new(
+        :application_id     => config['app']['id'],
+        :application_secret => config['app']['secret'],
+        :token              => access_token.token
+      )
+
+      @myid = @client.me.feed['data'][0]['from']['id'].to_i
+      @log.debug "my id: #{@myid}"
+    rescue Exception => e
+      @log.error e.inspect
+      e.backtrace.each do |l|
+        @log.error "\t#{l}"
+      end
+    end
   end
 
   def on_user(m)
     super
     post @prefix, JOIN, main_channel
-    post server_name, MODE, main_channel, "+o", @prefix.nick
+    post server_name, MODE, main_channel, '+o', @prefix.nick
 
     @real, *@opts = @opts.name || @real.split(/\s+/)
     @opts = @opts.inject({}) {|r,i|
-      key, value = i.split("=")
+      key, value = i.split('=')
       r.update(key => value)
     }
 
     @timeline = []
     @check_friends_thread = Thread.start do
-      loop do
+#      loop do
         begin
           check_friends
-        rescue ApiFailed => e
-          @log.error e.inspect
         rescue Exception => e
           @log.error e.inspect
           e.backtrace.each do |l|
             @log.error "\t#{l}"
           end
         end
-        sleep freq(@ratio[:friends] / @footing)
-      end
+#        sleep freq(@ratio[:friends] / @footing)
+#      end
     end
 
     @check_timeline_thread = Thread.start do
@@ -78,15 +109,13 @@ class FacebookIrcGateway < Net::IRC::Server::Session
       loop do
         begin
           check_timeline
-        rescue ApiFailed => e
-          @log.error e.inspect
         rescue Exception => e
           @log.error e.inspect
           e.backtrace.each do |l|
             @log.error "\t#{l}"
           end
         end
-        sleep 60
+        sleep @config['client']['wait'].to_i || 60
       end
     end
   end
@@ -97,7 +126,16 @@ class FacebookIrcGateway < Net::IRC::Server::Session
 
   def on_privmsg(m)
     super
-    @client.me.feed(:create, :message => m[1])
+    begin
+      ret = @client.me.feed(:create, :message => m[1])
+      post server_name, NOTICE, main_channel, "#{m[1]} (#{ret.to_s})"
+    rescue Exception => e
+      post server_name, NOTICE, main_channel, '投稿に失敗しました'
+      @log.error e.inspect
+      e.backtrace.each do |l|
+        @log.error "\t#{l}"
+      end
+    end
   end
 
   def on_ctcp(target, message)
@@ -119,121 +157,147 @@ class FacebookIrcGateway < Net::IRC::Server::Session
   def check_friends
     first = true unless @friends
     @friends ||= []
-    friends = @client.me.friends['data'].map{|i| {'name' => i['name'].gsub(/\s+/,''), 'id' => i['id']} }
+    friends = @client.me.friends['data'].map do |i|
+      {
+        'name' => i['name'].gsub(/\s+/,''), 
+        'id'   => i['id'].to_i
+      }
+    end
 
-    if first && !@opts.key?("athack")
+    if first
       @friends = friends
-      post server_name, RPL_NAMREPLY,   @nick, "=", main_channel, @friends.map{|i| "@#{i["name"]}" }.join(" ")
-      post server_name, RPL_ENDOFNAMES, @nick, main_channel, "End of NAMES list"
+      post server_name, RPL_NAMREPLY,   @nick, '=', main_channel, @friends.map{|i| "@#{i['name']}" }.join(' ')
+      post server_name, RPL_ENDOFNAMES, @nick, main_channel, 'End of NAMES list'
     else
-      prv_friends = @friends.map {|i| i["name"] }
-      now_friends = friends.map {|i| i["name"] }
+      prv_friends = @friends.map {|i| i['name'] }
+      now_friends = friends.map {|i| i['name'] }
 
       (now_friends - prv_friends).each do |join|
-        join = "@#{join}" if @opts.key?("athack")
+        join = "@#{join}"
         post "#{join}!#{join}@#{api_base.host}", JOIN, main_channel
       end
+
       (prv_friends - now_friends).each do |part|
-        part = "@#{part}" if @opts.key?("athack")
-        post "#{part}!#{part}@#{api_base.host}", PART, main_channel, ""
+        part = "@#{part}"
+        post "#{part}!#{part}@#{api_base.host}", PART, main_channel, ''
       end
       @friends = friends
     end
   end
-
 
   def check_timeline
     begin
       db = SDBM.open("#{Dir.tmpdir}/#{@real}.db", 0666)
       @client.me.home['data'].reverse.each do |d|
         id = d['id']
+        # 重複チェック
         if db.include?(id)
           next
         else
-          db[id] = "1"
+          db[id] = '1'
         end
-        post d['from']['name'].gsub(/\s+/, ''), PRIVMSG, main_channel, "#{d['message']}"
+
+        # 自分の発言の場合
+        if @myid == d['from']['id'].to_i
+          next
+        end
+
+        message     = d['message']
+        app_name    = d['application']['name'] if d['application']
+        name        = d['from']['name'].gsub(/\s+/, '')
+        link        = d['link']
+        caption     = d['caption']
+        description = d['description']
+        comments    = d['comments']['data'] if d['comments']
+
+        next unless name
+  
+        if message
+          mes = "#{message} "
+        else
+          mes = ''
+        end
+
+        if caption
+          if mes != ''
+            mes += '/ '
+          end
+          mes += "#{caption} "
+        end
+
+        if description
+          if mes != ''
+            mes += '/ '
+          end
+          mes += "#{description} "
+        end
+
+        mes += " #{link} " if link
+
+        if app_name
+          mes += "(#{app_name}) "
+        else
+          mes += '(web) '
+        end
+
+        post name, PRIVMSG, main_channel, mes
+
+        comments.each do |comment|
+          post name, PRIVMSG, main_channel, "(#{comment['from']['name'].gsub(/\s+/, '')}) >> #{comment['message']}"
+        end if comments
       end
     rescue Exception => e
       @log.error e.inspect
     ensure
       db.close rescue nil
     end
-    sleep 60
   end
 end
 
 if __FILE__ == $0
-  require "optparse"
+  require 'optparse'
 
   opts = {
-  :port       => 16822,
-  :host       => "localhost",
-  :log        => nil,
-  :debug      => false,
-  :foreground => false,
+    :port => 16822,
+    :host => 'localhost',
+    :log  => nil,
+    :config => 'config.yaml',
   }
 
   OptionParser.new do |parser|
     parser.instance_eval do
-      self.banner  = <<-EOB.gsub(/^\t+/, "")
+      self.banner  = <<-EOB.gsub(/^\t+/, '')
         Usage: #{$0} [opts]
 
       EOB
 
-      separator ""
+      separator ''
 
-      separator "Options:"
-      on("-p", "--port [PORT=#{opts[:port]}]", "port number to listen") do |port|
+      separator 'Options:'
+      on('-p', "--port [PORT=#{opts[:port]}]", 'port number to listen') do |port|
         opts[:port] = port
       end
 
-      on("-h", "--host [HOST=#{opts[:host]}]", "host name or IP address to listen") do |host|
+      on('-h', "--host [HOST=#{opts[:host]}]", 'host name or IP address to listen') do |host|
         opts[:host] = host
       end
 
-      on("-l", "--log LOG", "log file") do |log|
+      on('-l', '--log LOG', 'log file') do |log|
         opts[:log] = log
       end
 
-      on("--debug", "Enable debug mode") do |debug|
-        opts[:log]   = $stdout
-        opts[:debug] = true
-      end
-
-      on("-f", "--foreground", "run foreground") do |foreground|
-        opts[:log]        = $stdout
-        opts[:foreground] = true
+      on('-c', "--config [CONF=#{opts[:config]}", 'config file') do |config|
+        opts[:config] = config
       end
 
       parse!(ARGV)
     end
   end
 
-  opts[:logger] = Logger.new(opts[:log], "daily")
-  opts[:logger].level = opts[:debug] ? Logger::DEBUG : Logger::INFO
+  opts[:logger] = Logger.new($stdout, 'daily')
+  opts[:logger].level = Logger::DEBUG
 
- def daemonize(foreground=false)
-    trap("SIGINT")  { exit! 0 }
-    trap("SIGTERM") { exit! 0 }
-    trap("SIGHUP")  { exit! 0 }
-    return yield if $DEBUG || foreground
-    Process.fork do
-      Process.setsid
-      Dir.chdir "/"
-      File.open("/dev/null") {|f|
-        STDIN.reopen  f
-        STDOUT.reopen f
-        STDERR.reopen f
-      }
-      yield
-    end
-    exit! 0
-  end
-
-  daemonize(opts[:debug] || opts[:foreground]) do
-    Net::IRC::Server.new(opts[:host], opts[:port], FacebookIrcGateway, opts).start
-  end
+  Net::IRC::Server.new(opts[:host], opts[:port], FacebookIrcGateway, opts).start
 end
 
 # Local Variables:
