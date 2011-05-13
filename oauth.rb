352ed88b576bee2020106de23b17da18bd7d8089
shunirr <shunirr@gmail.com>
2011-05-13 15:01:36 +0900

    add default app_id and app_secret

--- oauth.rb
+++ oauth.rb
@@ -3,13 +3,25 @@ require 'rubygems'
 require 'oauth'
 require 'facebook_oauth'
 
-#config = YAML.load(open('config.yaml').read)
+DEFAULT_APP_ID = '221646024527845'
+DEFAULT_APP_SECRET = '012749b22fcc3111ea88760c209cdb27'
+
 config = {'app' => {}}
 
-print "Input your Application ID: "
-config['app']['id'] = gets.chomp
-print "Input your Application Secret: "
-config['app']['secret'] = gets.chomp
+print "Input your Application ID: (Press enter, if use default Application ID)"
+app_id = gets.chomp
+if app_id == ''
+  config['app']['id']     = DEFAULT_APP_ID
+  config['app']['secret'] = DEFAULT_APP_SECRET
+else
+  config['app']['id'] = app_id
+end
+
+unless config['app']['secret']
+  print "Input your Application Secret: "
+  config['app']['secret'] = gets.chomp
+end
+
 config['app']['callback'] = 'http://www.facebook.com/connect/login_success.html'
 client = FacebookOAuth::Client.new(
     :application_id     => config['app']['id'],
@@ -21,8 +33,8 @@ client = FacebookOAuth::Client.new(
 puts "---"
 puts client.authorize_url(:scope => 'offline_access, publish_stream, user_status, read_stream')
 puts "---"
-print "Please access this URL, and Paste param 'code': "
-code = gets.chomp
+print "Please access this URL, and Paste new URL: "
+code = gets.chomp.split("code=").last
 
 puts "Paste this to your Pit"
 puts <<EOS
