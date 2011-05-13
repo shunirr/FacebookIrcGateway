7309528cd9c0c741f5a16f4bb0ba6603e4de87ef
shunirr <shunirr@gmail.com>
2011-05-13 18:43:27 +0900

    ちょこっと修正

--- fig.rb
+++ fig.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
 $LOAD_PATH << 'lib'
-$LOAD_PATH << '../lib'
-
 $KCODE = 'u' unless defined? ::Encoding
 
 require 'rubygems'
 require 'net/irc'
 require 'pit'
+require 'optparse'
+
 require 'facebook_irc_gateway'
 
 CONFIG = Pit.get("facebok_irc_gateway", :require => {
@@ -17,45 +17,41 @@ CONFIG = Pit.get("facebok_irc_gateway", :require => {
       'code' => 'Your Authorization Code'
 })
 
-if __FILE__ == $0
-  require 'optparse'
+opts = {
+  :port => 16822,
+  :host => 'localhost',
+  :log  => nil,
+}
 
-  opts = {
-    :port => 16822,
-    :host => 'localhost',
-    :log  => nil,
-  }
+OptionParser.new do |parser|
+  parser.instance_eval do
+    self.banner  = <<-EOB.gsub(/^\t+/, '')
+      Usage: #{$0} [opts]
 
-  OptionParser.new do |parser|
-    parser.instance_eval do
-      self.banner  = <<-EOB.gsub(/^\t+/, '')
-        Usage: #{$0} [opts]
+    EOB
 
-      EOB
+    separator ''
 
-      separator ''
-
-      separator 'Options:'
-      on('-p', "--port [PORT=#{opts[:port]}]", 'port number to listen') do |port|
-        opts[:port] = port
-      end
-
-      on('-h', "--host [HOST=#{opts[:host]}]", 'host name or IP address to listen') do |host|
-        opts[:host] = host
-      end
-
-      on('-l', '--log LOG', 'log file') do |log|
-        opts[:log] = log
-      end
-
-      parse!(ARGV)
+    separator 'Options:'
+    on('-p', "--port [PORT=#{opts[:port]}]", 'port number to listen') do |port|
+      opts[:port] = port
     end
+
+    on('-h', "--host [HOST=#{opts[:host]}]", 'host name or IP address to listen') do |host|
+      opts[:host] = host
+    end
+
+    on('-l', '--log LOG', 'log file') do |log|
+      opts[:log] = log
+    end
+
+    parse!(ARGV)
   end
-
-  opts[:logger] = Logger.new($stdout, 'daily')
-  opts[:logger].level = Logger::DEBUG
-  opts[:pit] = @pit
-
-  Net::IRC::Server.new(opts[:host], opts[:port], FacebookIrcGateway::Server, opts).start
 end
+
+opts[:logger] = Logger.new($stdout, 'daily')
+opts[:logger].level = Logger::DEBUG
+opts[:pit] = @pit
+
+Net::IRC::Server.new(opts[:host], opts[:port], FacebookIrcGateway::Server, opts).start
 
