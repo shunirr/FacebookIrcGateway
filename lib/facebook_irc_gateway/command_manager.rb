eaaa6c9ea0d11789025d26433591ad62da33688a
mashiro <y.mashiro@gmail.com>
2011-05-25 03:55:05 +0900

    さやかちゃん

--- lib/facebook_irc_gateway/command_manager.rb
+++ lib/facebook_irc_gateway/command_manager.rb
@@ -1,3 +1,4 @@
+require 'i18n'
 
 module FacebookIrcGateway
   class CommandManager
@@ -12,25 +13,50 @@ module FacebookIrcGateway
       register_builtins
     end
 
-    def register(name, options = {}, &block)
-      name = name.to_s
-      @command_map[name] ||= []
-      @command_map[name] << {:block => block, :options => DEFAULT_OPTIONS.merge(options)}
+    def register(names, options = {}, &block)
+      [names].flatten.each do |name|
+        name = name.to_s.downcase
+        @command_map[name] ||= []
+        @command_map[name] << {:block => block, :options => DEFAULT_OPTIONS.merge(options)}
+      end
     end
 
     def process(channel, message)
       cancel = false
       name, tid, args = message.split(/\s+/, 3)
+      tid.downcase! if tid
 
       commands = @command_map[name] || []
       if not commands.empty?
         id, status = @session.typablemap[tid]
-        commands.each do |command|
-          block = command[:block]
-          options = command[:options]
-          next if tid.nil? and options[:tid]
-          block.call :id => id, :status => status, :tid => tid, :args => args, :channel => channel, :session => @session
+        if status.nil?
+          # 残念、さやかちゃんでした！
+          channel.notice I18n.t('server.invalid_typablemap')
           cancel = true
+        else
+          commands.each do |command|
+            block = command[:block]
+            options = command[:options]
+            next if tid.nil? and options[:tid]
+
+            begin
+              block.call(
+                :id => id,
+                :status => status,
+                :tid => tid,
+                :args => args,
+                :channel => channel,
+                :session => @session
+              )
+            rescue Exception => e
+              channel.notice e.inspect
+              e.backtrace.each do |l|
+                channel.notice "\t#{l}"
+              end
+            end
+
+            cancel = true
+          end
         end
       end
 
@@ -41,14 +67,17 @@ module FacebookIrcGateway
 
     def register_builtins
       register :re do |options|
-        p "re: #{options}"
-        session = options[:session]
-        status = options[:status]
-        args = options[:args]
+        session, channel, status, args = options.values_at(:session, :channel, :status, :args)
         session.api.status(status['id']).comments(:create, :message => args)
+        channel.notice "re: #{status}"
+      end
+
+      register [:like, :fav, :arr] do |options|
+        session, channel, id, status = options.values_at(:session, :channel, :id, :status)
+        session.api.status(id).likes(:create)
+        channel.notice "like: #{status}"
       end
     end
-
   end
 end
 
