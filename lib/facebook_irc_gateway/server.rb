67166017b30bd29a49805f18ef6f8cf336239d68
shunirr <m@s5r.jp>
2014-10-15 14:07:36 +0900

    SSLv3 で繋がらなくなったので TLSv1 を使うように変更

--- lib/facebook_irc_gateway/server.rb
+++ lib/facebook_irc_gateway/server.rb
@@ -5,7 +5,7 @@ require 'openssl'
 class OpenSSL::SSL::SSLContext
   if const_defined? :DEFAULT_PARAMS
     const_set(:DEFAULT_PARAMS, remove_const(:DEFAULT_PARAMS).merge({
-      :ssl_version => 'SSLv3',
+      :ssl_version => 'TLSv1',
       :verify_mode => OpenSSL::SSL::VERIFY_NONE,
     }))
   end
